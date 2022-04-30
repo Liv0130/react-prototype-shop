1dabb68d0ca7c1caac4811b530a083395b0c1e3b
Liv0130 <yjjhsh27@gmail.com>
2022-04-30 23:36:05 +0900

    Added total

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,9 +1,21 @@
+import { useMemo } from 'react';
+import useActions from "../hooks/useActions";
 import useOrders from "../hooks/useOrders";
 import usePrototypes from "../hooks/usePrototypes";
 
 export default function Order() {
   const orders = useOrders();
   const prototypes = usePrototypes();
+  const { remove } = useActions();
+
+  const totalPrice = useMemo(() => {
+    return orders.map(order => {
+      const { id, quantity } = order;
+      const prototype = prototypes.find(p => p.id === id);
+      return prototype.price * quantity;
+    }).reduce((l, r) => l + r, 0);
+  }, [orders, prototypes]);
+
   if (orders.length === 0) {
   return (
   <aside>
@@ -16,17 +28,42 @@ export default function Order() {
 }
 return (
   <aside>
+    <div className="order">
     <div className="body">
       {orders.map(order => {
         const { id } = order;
         const prototype = prototypes.find(p => p.id === id);
+        const click = () => {
+          remove(id);
+        }
         return (
         <div className="item" key={id}>
             <div className="img">
-              <vidoe src={prototype.thumbnail} />
+              <video src={prototype.thumbnail} />
+            </div>
+            <div className="content"> 
+                <p className="title">
+                  {prototype.title} x {order.quantity}
+                </p>
+            </div>
+            <div className="action"> 
+                <p className="price">
+                  $ {prototype.price} x {order.quantity}
+                </p>
+                <button className="btn btn--link" onClick={click}><i className="icon icon--cross"></i> </button>
             </div>
           </div>)
       })}
+    </div>
+    <div className="total">
+      <hr />
+      <div className="item">
+        <div className="content">Total</div>
+        <div className="action">
+          <div className="price">$ {totalPrice}</div>
+        </div>
+      </div>
+    </div>
     </div>
   </aside>
   )
