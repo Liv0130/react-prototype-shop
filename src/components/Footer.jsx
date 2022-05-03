b8de30f3d58a99d1e1c0d111ce09dbd0578e75c0
Liv0130 <yjjhsh27@gmail.com>
2022-05-03 23:21:32 +0900

    Organised footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,15 @@
+const footerstyle = {
+  display: "flex",
+  justifyContent: 'center',
+}
+
 export default function Footer() {
   return (
-    <footer>
+    <footer style={footerstyle}>
       <p>© 2022 Olivia Oh. All rights reserved.</p>
     </footer>
   );
 }
+
+
 
